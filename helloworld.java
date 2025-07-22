163c1d59fc7f8d065620308e01b9dbd4325173cb
Samuel K. Asamoah <asamoahs@roehampton.ac.uk>
2025-07-22 15:30:14 +0100

    Simple program to demonstrate print statements in JAVA

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("This is a simple Java program which prints statements to the console.");
+        System.out.println("Let's learn and have fun with coding!");
+        System.out.println("Goodbye!");
+    }
+}
